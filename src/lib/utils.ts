aabc50197f77bef70f195467e2ce7f50bf236cec
NIYOMUGISHA Obed <obed6@duck.com>
2025-09-26 15:40:16 +0200

    feat: refactor social media components and integrate OpenGraph and TwitterCard for improved SEO

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,3 +5,14 @@ export function replaceSpaces(text: string): string {
 export function replaceUnderscores(text: string): string {
   return text.replace(/_+/g, ' ');
 }
+
+export function extractMetaImageTag(metaTag: string): { url: string } | null {
+  const regex = /<meta\s+property="og:image"\s+content="([^"]+)"/;
+  const match = metaTag.match(regex);
+
+  if (match && match[1]) {
+    return { url: match[1] };
+  }
+
+  return null;
+}
