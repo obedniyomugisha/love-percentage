574847bc9c1f58e5ded2d7c9f8591211a0ee8680
NIYOMUGISHA Obed <obed6@duck.com>
2025-10-21 22:49:07 +0200

    feat: enhance button component with collapsible actions and improved transition effects

--- src/components/Results/Buttons.svelte
+++ src/components/Results/Buttons.svelte
@@ -7,6 +7,7 @@
     labels,
   } from '@lib/buttons';
   import Button from '@components/Results/Button.svelte';
+  import { fade } from 'svelte/transition';
 
   let {
     first = $bindable(),
@@ -25,11 +26,41 @@
     () => copyToClipboard({ origin, first, second }),
     () => goHome(),
   ];
+
+  let divHeight = $state(48);
+  let collapse = $state(true);
 </script>
 
-<div class="flex gap-2 flex-wrap my-7 justify-center">
-  {#each labels as label, i}
-    {@const onclick = functions[i]}
-    <Button text={label} {onclick}></Button>
-  {/each}
-</div>
+{#if divHeight > 48}
+  {@const id = 'collapse'}
+
+  <input
+    type="checkbox"
+    class="size-5 hidden"
+    name={id}
+    {id}
+    bind:checked={collapse}
+  />
+
+  <label
+    class="text-2xl cursor-pointer font-semibold bg-pink-50 py-1 block text-center w-full"
+    for={id}>{!collapse ? 'Hide' : 'Show'} Actions</label
+  >
+{/if}
+
+{#if divHeight <= 48 || (divHeight > 48 && !collapse)}
+  {@render Div()}
+{/if}
+
+{#snippet Div()}
+  <div
+    bind:clientHeight={divHeight}
+    class="flex gap-2 flex-wrap my-7 justify-center"
+    transition:fade
+  >
+    {#each labels as label, i}
+      {@const onclick = functions[i]}
+      <Button text={label} {onclick}></Button>
+    {/each}
+  </div>
+{/snippet}
